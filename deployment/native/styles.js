2dc8b8ec274ea1901e7f9b8f6c6c73acf817e215
tarik.subasi <tarik.subasi@hotmail.com>
2023-08-12 14:50:57 +0300

    1.4 - Open Empty Blank App

---
+++ deployment/native/styles.js
@@ -0,0 +1,11 @@
+import * as style0 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/administration/native/main";
+import * as style1 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/webactions/native/main";
+import * as style3 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/atlas_core/native/main";
+import * as style4 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/feedbackmodule/native/main";
+import * as style5 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/myfirstmodule/native/main";
+import * as style6 from "C:/Users/TKA/Documents/Mendix/QuizApp/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
