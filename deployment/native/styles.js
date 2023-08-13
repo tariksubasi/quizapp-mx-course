7ba0a48bacf8af9c7fa475a89238cc53b6722a53
tarik.subasi <tarik.subasi@hotmail.com>
2023-08-13 17:16:57 +0300

    3.1 - Setting up anonymous user setting from project security

--- deployment/native/styles.js
+++ deployment/native/styles.js
@@ -6,8 +6,9 @@ import * as style4 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/atlas
 import * as style5 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/feedbackmodule/native/main";
 import * as style6 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/quizapp/native/main";
 import * as style7 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/extensions/native/main";
-import * as style8 from "C:/Users/TKA/Documents/Mendix/QuizApp/theme/native/main";
+import * as style8 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/anonymous/native/main";
+import * as style9 from "C:/Users/TKA/Documents/Mendix/QuizApp/theme/native/main";
 
 import { flatten } from "mendix/native";
 
-module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7, style8]);
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7, style8, style9]);
