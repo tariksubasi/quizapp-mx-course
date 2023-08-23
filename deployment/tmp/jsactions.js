829e53256f7109531dca52ddf298a337940febff
tarik.subasi <tarik.subasi@hotmail.com>
2023-08-23 12:46:06 +0300

    5.11 - Text Change Behaviour - Fixing Bugs With JS

--- deployment/tmp/jsactions.js
+++ deployment/tmp/jsactions.js
@@ -1,2 +1,4 @@
 export const NanoflowCommons$ShowProgress = async () => (await import("C:/Users/TKA/Documents/Mendix/QuizApp/javascriptsource/nanoflowcommons/actions/ShowProgress")).ShowProgress;
 export const NanoflowCommons$HideProgress = async () => (await import("C:/Users/TKA/Documents/Mendix/QuizApp/javascriptsource/nanoflowcommons/actions/HideProgress")).HideProgress;
+export const Builder$JS_Fix_OnQuestionChange = async () => (await import("C:/Users/TKA/Documents/Mendix/QuizApp/javascriptsource/builder/actions/JS_Fix_OnQuestionChange")).JS_Fix_OnQuestionChange;
+export const Builder$JS_Fix_OnQuestionBodyChange = async () => (await import("C:/Users/TKA/Documents/Mendix/QuizApp/javascriptsource/builder/actions/JS_Fix_OnQuestionBodyChange")).JS_Fix_OnQuestionBodyChange;
