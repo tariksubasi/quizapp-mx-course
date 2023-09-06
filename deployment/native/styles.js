7666a835fa6281fe379d5821de40c5b6137f1c00
tarik.subasi <tarik.subasi@hotmail.com>
2023-09-06 19:48:23 +0300

    8.2 - Writing First Query

--- deployment/native/styles.js
+++ deployment/native/styles.js
@@ -10,8 +10,9 @@ import * as style8 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/solve
 import * as style9 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/quizapp/native/main";
 import * as style10 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/extensions/native/main";
 import * as style11 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/anonymous/native/main";
-import * as style12 from "C:/Users/TKA/Documents/Mendix/QuizApp/theme/native/main";
+import * as style12 from "C:/Users/TKA/Documents/Mendix/QuizApp/themesource/datastore/native/main";
+import * as style13 from "C:/Users/TKA/Documents/Mendix/QuizApp/theme/native/main";
 
 import { flatten } from "mendix/native";
 
-module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7, style8, style9, style10, style11, style12]);
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7, style8, style9, style10, style11, style12, style13]);
